91a15154bb001544cbfa756800a95bb8adf6bfe4
Coala Jedi <coalajedi.ios@gmail.com>
2022-12-26 17:29:19 -0300

    feat: :sparkles: introduce cancel notification feature
    this commit adds the Cancel notification feature with tests

--- src/app/repositories/notifications-repository.ts
+++ src/app/repositories/notifications-repository.ts
@@ -2,4 +2,8 @@ import { Notification } from '../entities/notification';
 
 export abstract class NotificationsRepository {
   abstract create(notifications: Notification): Promise<void>;
+
+  abstract findById(notificationId: string): Promise<Notification | null>;
+
+  abstract save(notification: Notification): Promise<void>;
 }
